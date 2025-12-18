3b639cdedb341a7332aa46666062cb8c623a3be0
Afnan Ghoni Brinto <24-60363-3@student.aiub.edu>
2025-12-18 11:24:01 +0600

    Add files via upload

---
+++ Arithmetic basic/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{int a,b;
+float result;
+cout<<"Give me the numbers you want to do "<<endl;
+cin>>a>>b;
+result=a+b;
+cout<<"The sum is :  "<<result<<endl;
+result=a-b;
+cout<<"The sub is :  "<<result<<endl;
+result=a*b;
+cout<<"The mylti is :  "<<result<<endl;
+result=a/b;
+cout<<"The div is :  "<<result<<endl;
+return 0;
+}
